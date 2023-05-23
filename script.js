47a79c13f4f5e24381720b74ff7e3ba1cc59fe0d
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-05-23 21:55:09 +0300

    working on dropdowns

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//how to place two unordered lists in one line?
+const dropdownTrigger = document.getElementById('dp1');
+const dropdownMenu = document.getElementById('dpm1');
+
+dropdownTrigger.addEventListener('mouseover', () => {
+    dropdownMenu.style.display = 'block'
+});
+
+dropdownTrigger.addEventListener('mouseleave', () => {
+    dropdownMenu.style.display ='none';
+});
+
+const dpt = document.getElementById('dp2');
+const ddm = document.getElementById('dpm2');
+
+dpt.addEventListener('mouseover', () => {
+    dpt.style.display = 'block';
+});
+
+ddm.addEventListener('mouseleave', () => {
+    ddm.style.display = 'none';
+});
